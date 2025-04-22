a7da9782f69ffa0baac4d42c5b114c095dcd4177
soheb85 <shoebkhan3145@gmail.com>
2025-04-22 22:12:56 +0530

    layout of dashboard auth.ts

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/dashboard/app-sidebar";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth"; 
 import { redirect } from "next/navigation";
 import Footer from "@/components/Footer";
 
